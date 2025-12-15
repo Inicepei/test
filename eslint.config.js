9dc064f47ddb98d042d7169a9f88134e6d6eb7bb
Mikhail <mikhailbelov737@gmail.com>
2025-12-15 14:20:07 +0100

    feat: add eslint and config for it

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "no-console": "warn",
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+    },
+  },
+];
